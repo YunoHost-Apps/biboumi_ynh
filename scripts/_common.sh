132c21b35e673106f5e44f7719590afb433e674b
Félix Piédallu <felix@piedallu.me>
2024-07-01 16:54:41 +0200

    Programmatic path to the metronome config dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,22 @@
 # PERSONAL HELPERS
 #=================================================
 
+_get_metronome_config_dir() {
+    if [[ $YNH_DEBIAN_VERSION == "bullseye" ]]; then
+        echo "/etc/metronome/conf.d"
+        return 0
+    fi
+
+    # multi_instance=false, there's only one
+    metronome_app=$(yunohost app list --json | jq '.apps[] | select(.id == "metronome")) | .id')
+    if (( ${#metronome_apps[@]} == 0 )); then
+        ynh_print_warn --message="Could not find any metronome app!"
+        return 1
+    fi
+
+    echo "$(yunohost app setting "$metronome_app" install_dir)/conf/conf.d"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
