8ba8aed6b4b7bbe9d78c959742ce125df27114ee
pitchum <pitchum@users.noreply.github.com>
2025-05-14 13:39:18 +0200

    Make prosody the default supported xmpp-server (but still support metronome) (#30)
    And also migrate scripts to helpers v2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,33 +1,71 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-#=================================================
-# PERSONAL HELPERS
-#=================================================
-
-_get_metronome_config_dir() {
-    if [[ $YNH_DEBIAN_VERSION == "bullseye" ]]; then
-        echo "/etc/metronome/conf.d"
-        return 0
-    fi
-
-    # multi_instance=false, there's only one
-    metronome_app=$(yunohost app list --json | jq -r '.apps[] | select(.id == "metronome") | .id')
-    if [[ -z "${metronome_app:-}" ]]; then
-        ynh_print_warn --message="Could not find any metronome app!"
-        return 1
-    fi
-
-    echo "$(ynh_app_setting_get --app="$metronome_app" --key="install_dir")/conf/conf.d"
+# Guess which xmpp server is running.
+_get_xmpp_app_name() {
+    for candidate in prosody metronome ; do
+        if systemctl is-active prosody -q ; then
+            echo $candidate
+            return 0
+        fi
+    done
+    ynh_die "Failed to guess the XMPP server software running"
 }
 
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
+# Compute the config filename where the XMPP component for biboumi is configured.
+_get_xmpp_server_config_filename() {
+    local xmpp_app=$1
+    if [ $xmpp_app = prosody ] ; then
+        echo "/etc/$xmpp_app/conf.d/$app.cfg.lua"
+    elif [ $xmpp_app = metronome ] ; then
+        echo "/etc/$xmpp_app/conf.d/$app.cfg.lua"
+    else
+        ynh_die "Failed to configure unsupported XMPP server $xmpp_app"
+    fi
+}
 
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
+# Configure biboumi as an XMPP component for the running XMPP server.
+_configure_xmpp_server() {
+    local xmpp_app=$1
+    config_filename=$(_get_xmpp_server_config_filename $xmpp_app)
+    if [ $xmpp_app = prosody ] ; then
+        ynh_config_add --template="prosody.cfg.lua" --destination="$config_filename"
+        chown prosody:prosody "/etc/$xmpp_app/conf.d/$app.cfg.lua"
+    elif [ $xmpp_app = metronome ] ; then
+        ynh_config_add --template="prosody.cfg.lua" --destination="$config_filename"
+        chown metronome:metronome "/etc/$xmpp_app/conf.d/$app.cfg.lua"
+    else
+        ynh_die "Failed to configure unsupported XMPP server $xmpp_app"
+    fi
+}
+
+_ynh_config_add_logrotate_for_biboumi() {
+    mkdir --parents /var/log/$app
+    chmod 750 /var/log/$app
+    chown _biboumi:_biboumi /var/log/$app
+
+    echo > /etc/logrotate.d/$app <<EOF
+/var/log/$app/$app.log {
+    # Rotate if the logfile exceeds 100Mo
+    size 100M
+    # Keep 12 old log maximum
+    rotate 12
+    # Compress the logs with gzip
+    compress
+    # Compress the log at the next cycle. So keep always 2 non compressed logs
+    delaycompress
+    # Copy and truncate the log to allow to continue write on it. Instead of moving the log.
+    copytruncate
+    # Do not trigger an error if the log is missing
+    missingok
+    # Do not rotate if the log is empty
+    notifempty
+    # Keep old logs in the same dir
+    noolddir
+}
+EOF
+    chmod 644 "/etc/logrotate.d/$app"
+}
